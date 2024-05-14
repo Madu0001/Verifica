f42810643d8123f2848adf7e07ffa33510070452
ArthurDezan <169094412+ArthurDezan@users.noreply.github.com>
2024-05-14 17:08:45 +0000

    arrumando o site

--- Tela inicial/Tela_inicial.js
+++
@@ -1,6 +0,0 @@
-const toggleModeButton = document.getElementById('toggleMode');
-const bodyElement = document.body;
-
-toggleModeButton.addEventListener('click', () => {
-    bodyElement.classList.toggle('dark-mode');
-});
